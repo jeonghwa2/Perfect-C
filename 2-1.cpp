8995d5386af48e0013506c4a741b25408084dc97
jeonghwa2 <jude5110@hanmail.net>
2018-05-07 00:31:53 +0900

    Add files via upload

---
+++ 2-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 100;
+	int b = 200; 
+
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a*b);
+	printf("%d\n", a / b);
+
+
+	return 0;
+}
